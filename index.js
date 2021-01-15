dfdedc30243b709a4b9e4d63080846c6f7a56d9b
Dexter Fitch <fitch.dexter@gmail.com>
2021-01-15 02:38:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // Write your solution in this file!
 const driver = Object;
+
+function updateDriverWithKeyAndValue(obj, key, val) {
+  const newDriver = "";
+  newDriver = Object.assign(obj, key, val);
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+}
+
+function deleteFromDriverByKey() {
+}
+
+function destructivelyDeleteFromDriverByKey() {
+}
